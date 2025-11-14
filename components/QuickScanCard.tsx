b00a4a3d987f1aec6c9900d685074fd549e02e0a
rania-debug <rania@securas.fr>
2025-11-14 15:25:38 +0100

    update9

--- components/QuickScanCard.tsx
+++ components/QuickScanCard.tsx
@@ -181,7 +181,7 @@ export function QuickScanCard() {
 
       const computed = computeRiskFromResult(scanRaw as ScanApiResult);
       const analyzerCms =
-        Array.isArray(analyzerData?.cms) && analyzerData?.cms?.length > 0
+        Array.isArray(analyzerData?.cms) && (analyzerData.cms?.length ?? 0) > 0
           ? formatCmsLabel(analyzerData.cms[0])
           : null;
 
